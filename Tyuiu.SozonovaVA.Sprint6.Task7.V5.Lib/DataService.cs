097f148aac7cec7e0477c4a4871eed2cc9ad7a28
Варвара Созонова <Tyuiu.SozonovaVA@outlook.com>
2025-12-24 03:26:52 +0500

    Task7

--- Tyuiu.SozonovaVA.Sprint6.Task7.V5.Lib/DataService.cs
+++ Tyuiu.SozonovaVA.Sprint6.Task7.V5.Lib/DataService.cs
@@ -5,44 +5,31 @@ namespace Tyuiu.SozonovaVA.Sprint6.Task7.V5.Lib
     {
         public int[,] GetMatrix(string path)
         {
-            var lines = File.ReadAllLines(path);
-            var matrix = new int[lines.Length, lines[0].Split(',').Length];
+            string[] lines = File.ReadAllLines(path);
+            int rows = lines.Length;
+            string[] firstLineValues = lines[0].Split(';');
+            int cols = firstLineValues.Length;
 
-            for (int i = 0; i < lines.Length; i++)
+            int[,] matrix = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
             {
-                var values = lines[i].Split(',').Select(int.Parse).ToArray();
-                for (int j = 0; j < values.Length; j++)
+                string[] values = lines[i].Split(';');
+
+                for (int j = 0; j < cols; j++)
                 {
-                    matrix[i, j] = values[j];
+                    matrix[i, j] = int.Parse(values[j]);
+                }
+            }
+            int lastColumnIndex = cols - 1;
+            for (int i = 0; i < rows; i++)
+            {
+                if (matrix[i, lastColumnIndex] < 2)
+                {
+                    matrix[i, lastColumnIndex] = 2;
                 }
             }
 
-            return matrix;
-        }
-
-       
-        public void SaveMatrix(int[,] matrix, string path)
-        {
-            var lines = new string[matrix.GetLength(0)];
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                var line = string.Join(",", Enumerable.Range(0, matrix.GetLength(1)).Select(j => matrix[i, j].ToString()));
-                lines[i] = line;
-            }
-
-            File.WriteAllLines(path, lines);
-        }
-
-        
-        public int[,] ProcessMatrix(int[,] matrix)
-        {
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                if (matrix[i, matrix.GetLength(1) - 1] < 2)
-                {
-                    matrix[i, matrix.GetLength(1) - 1] = 2;
-                }
-            }
             return matrix;
         }
 
