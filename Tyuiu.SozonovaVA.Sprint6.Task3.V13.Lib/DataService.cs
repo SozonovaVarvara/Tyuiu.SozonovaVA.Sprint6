c83efbb16d42fd56d58d8a92a370cfaf2732ba8f
Варвара Созонова <Tyuiu.SozonovaVA@outlook.com>
2025-12-24 01:54:37 +0500

    Task3

---
+++ Tyuiu.SozonovaVA.Sprint6.Task3.V13.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.SozonovaVA.Sprint6.Task3.V13.Lib
+{
+    public class DataService : ISprint6Task3V13
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = i + 1; j < matrix.GetLength(0); j++)
+                {
+                    if (matrix[j, 1] < matrix[i, 1])
+                    {
+                        int temp = matrix[i, 1];
+                        matrix[i, 1] = matrix[j, 1];
+                        matrix[j, 1] = temp;
+                    }
+                }
+            }
+            return matrix;
+        }
+    }
+}
---
+++ Tyuiu.SozonovaVA.Sprint6.Task3.V13/FormMain.cs
@@ -0,0 +1,11 @@
+using Tyuiu.SozonovaVA.Sprint6.Task3.V13.Lib;
+namespace Tyuiu.SozonovaVA.Sprint6.Task3.V13
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
