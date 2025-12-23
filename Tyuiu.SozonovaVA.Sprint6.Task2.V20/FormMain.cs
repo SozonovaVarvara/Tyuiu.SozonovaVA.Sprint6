e743c6560ec042b6121182ca8b65506997b81aef
Варвара Созонова <Tyuiu.SozonovaVA@outlook.com>
2025-12-24 01:45:15 +0500

    Task2

---
+++ Tyuiu.SozonovaVA.Sprint6.Task2.V20/FormMain.cs
@@ -0,0 +1,16 @@
+using Tyuiu.SozonovaVA.Sprint6.Task2.V20.Lib;
+namespace Tyuiu.SozonovaVA.Sprint6.Task2.V20
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
